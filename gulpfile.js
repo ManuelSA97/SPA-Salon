3070085ebe6fca98fb76347133fc6a8b6f1665d0
Manuel Sandi <manuelsandi97@hotmail.com>
2024-02-15 15:10:54 -0600

    Cambio de formato de imagenes a .avif

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const cssnano = require('cssnano');
 
 // Imagenes
 const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
+//const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
 function css( done ) {
@@ -29,14 +29,14 @@ function imagenes() {
         .pipe( dest('build/img') )
 }
 
-function versionWebp() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
-}
+// function versionWebp() {
+//     const opciones = {
+//         quality: 50
+//     }
+//     return src('src/img/**/*.{png,jpg}')
+//         .pipe( webp( opciones ) )
+//         .pipe( dest('build/img') )
+// }
 function versionAvif() {
     const opciones = {
         quality: 50
@@ -55,6 +55,6 @@ function dev() {
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
+//exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series( imagenes, versionAvif, css, dev  );
