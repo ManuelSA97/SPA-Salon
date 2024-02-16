4829dfc0f740398819a22e7f1df729a55b642dfc
Manuel Sandi <88011893+ManuelSA97@users.noreply.github.com>
2024-02-15 23:46:54 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,3 @@
-// window.swiper = new Swiper({
-//     el: '.swiper-wrapper',
-//     slideClass: 'swiper-slide',
-//     createElements: true,
-//     loop: true,
-//     autoplay: {
-//         delay: 3000
-//     },
-//     navigation: {
-//         el: '.swiper-wrapper',
-//       },
-//     navigation: true,
-//     pagination: true,
-//     effect: "cube",
-//       grabCursor: true,
-//       cubeEffect: {
-//         shadow: true,
-//         slideShadows: true,
-//         shadowOffset: 20,
-//         shadowScale: 0.94,
-//       },
-    
-// });
-
 var swiper = new Swiper(".swiper", {
     effect: "cube",
     grabCursor: true,
